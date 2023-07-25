284e297bba9b66d9fc0c4372a4a1d5437ac75f84
x2p <otmanhero00@gmail.com>
2023-07-25 09:18:33 -0400

    Fix Bug Calnder && Form Export

--- src/app/components/FormExport.js
+++ src/app/components/FormExport.js
@@ -1,7 +1,8 @@
 import {useState} from "react";
 
 
-const FormExport = () => {
+const FormExport = (props, ref) => {
+
     const [formData, setFormData] = useState({
         date: '',
         receiver: '',
@@ -9,23 +10,31 @@ const FormExport = () => {
         notes: [{ number: 1, dateOfNote: '' }],
     });
 
-    const handleAddNote = () => {
-        setFormData((prevData) => ({
-            ...prevData,
-            notes: [...prevData.notes, { number: prevData.notes.length + 1, dateOfNote: '' }],
-        }));
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+
+        // Check if all fields are filled
+        if (!formData.date || !formData.receiver || !formData.object) {
+            alert("Please fill out all fields.");
+            return;
+        }
+
+        // Check if the date is in the past
+        const currentDate = new Date().toISOString().split("T")[0];
+        if (formData.date > currentDate) {
+            alert("Date must be in the past.");
+            return;
+        }
+
+        // Handle form submission here
+        console.log('Form data:', formData);
     };
 
     const handleNoteChange = (index, value) => {
         const updatedNotes = [...formData.notes];
         updatedNotes[index].dateOfNote = value;
         setFormData({ ...formData, notes: updatedNotes });
-    };
-
-    const handleSubmit = (e) => {
-        e.preventDefault();
-        // Handle form submission here
-        console.log('Form data:', formData);
     };
 
 
@@ -40,8 +49,7 @@ const FormExport = () => {
                     id="date"
                     value={formData.date}
                     onChange={(e) => setFormData({ ...formData, date: e.target.value })}
-                    className="block w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50"
-                />
+                    className="block  mt-2 w-full placeholder-gray-400/70 dark:placeholder-gray-500 rounded-lg border border-gray-200 bg-white px-5 py-2.5 text-gray-700 focus:border-blue-400 focus:outline-none focus:ring focus:ring-blue-300 focus:ring-opacity-40 dark:border-gray-600 dark:bg-gray-900 dark:text-gray-300 dark:focus:border-blue-300"                />
             </div>
             <div>
                 <label htmlFor="receiver" className="block font-medium">
@@ -52,8 +60,7 @@ const FormExport = () => {
                     id="receiver"
                     value={formData.receiver}
                     onChange={(e) => setFormData({ ...formData, receiver: e.target.value })}
-                    className="block w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50"
-                />
+                    className="block  mt-2 w-full placeholder-gray-400/70 dark:placeholder-gray-500 rounded-lg border border-gray-200 bg-white px-5 py-2.5 text-gray-700 focus:border-blue-400 focus:outline-none focus:ring focus:ring-blue-300 focus:ring-opacity-40 dark:border-gray-600 dark:bg-gray-900 dark:text-gray-300 dark:focus:border-blue-300"                />
             </div>
             <div>
                 <label htmlFor="object" className="block font-medium">
@@ -64,8 +71,7 @@ const FormExport = () => {
                     id="object"
                     value={formData.object}
                     onChange={(e) => setFormData({ ...formData, object: e.target.value })}
-                    className="block w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50"
-                />
+                    className="block  mt-2 w-full  placeholder-gray-400/70 dark:placeholder-gray-500 rounded-lg border border-gray-200 bg-white px-4 h-32 py-2.5 text-gray-700 focus:border-blue-400 focus:outline-none focus:ring focus:ring-blue-300 focus:ring-opacity-40 dark:border-gray-600 dark:bg-gray-900 dark:text-gray-300 dark:focus:border-blue-300"/>
             </div>
             <div>
                 <h3 className="font-medium">Notes:</h3>
@@ -79,24 +85,18 @@ const FormExport = () => {
                             id={`note-${index}`}
                             value={note.dateOfNote}
                             onChange={(e) => handleNoteChange(index, e.target.value)}
-                            className="block w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50"
-                        />
+                            className="block  mt-2 w-full placeholder-gray-400/70 dark:placeholder-gray-500 rounded-lg border border-gray-200 bg-white px-5 py-2.5 text-gray-700 focus:border-blue-400 focus:outline-none focus:ring focus:ring-blue-300 focus:ring-opacity-40 dark:border-gray-600 dark:bg-gray-900 dark:text-gray-300 dark:focus:border-blue-300"                        />
                     </div>
                 ))}
-                <button
-                    type="button"
-                    onClick={handleAddNote}
-                    className="px-4 py-2 bg-indigo-600 text-white rounded-md hover:bg-indigo-700 focus:outline-none focus:ring focus:ring-indigo-200 focus:ring-opacity-50"
+
+            </div>
+            <div className="text-right"> {/* Add this container div with "text-left" class */}
+                <button onSubmit={handleSubmit}
+                    className="px-6 py-2 font-medium tracking-wide text-white capitalize transition-colors duration-300 transform bg-blue-600 rounded-lg hover:bg-blue-500 focus:outline-none focus:ring focus:ring-blue-300 focus:ring-opacity-80"
                 >
-                    Add Note
+                    Submit
                 </button>
             </div>
-            <button
-                type="submit"
-                className="px-4 py-2 bg-indigo-600 text-white rounded-md hover:bg-indigo-700 focus:outline-none focus:ring focus:ring-indigo-200 focus:ring-opacity-50"
-            >
-                Submit
-            </button>
         </form>
     );
 };
--- src/app/page.js
+++ src/app/page.js
@@ -1,11 +1,13 @@
 'use client';
-import React, { useState } from 'react';
+import React, {useRef, useState} from 'react';
 import {Calendar, Drawer, Dropdown, Menu, Modal, Space} from 'antd';
 import dayjs from 'dayjs';
 import Image from 'next/image';
 import { CheckCircleOutlined } from '@ant-design/icons';
 import Cartb from "../app/components/Carditem";
 import points from "../../icons/points.svg";
+import moment from 'moment';
+
 import FormExport from "@/app/components/FormExport";
 
 
@@ -15,24 +17,26 @@ const Home = () => {
     const [visible, setVisible] = useState(false);
     const [Dataofday, setDataofday] = useState([]);
     const [isModalVisible, setIsModalVisible] = useState(false);
-
+    const formRef = useRef(null);
 
 
     // Sample event data
     const events = [
+
         {
             title: 'Event 1',
-            start: '2023-07-01',
-            type: 'export',
-            end: '2023-07-10',
+            type: 'import',
+            start: '2023-07-20',
+            end: '2023-07-27',
         },
         {
             title: 'Event 2',
-            start: '2021-09-03',
             type: 'import',
-            end: '2021-09-05',
-        },
-        // Add more events as needed
+            start: '2023-07-20',
+            end: '2023-07-25',
+        }
+
+
     ];
 
     const onSelect = (newValue) => {
@@ -51,35 +55,35 @@ const Home = () => {
 
         setDataofday(events.filter((event) => date >= event.start && date <= event.end));
     };
-
     const handleCloseDrawer = () => {
         setVisible(false);
     };
 
     const dateCellRender = (date) => {
+        const dateString = date.format('YYYY-MM-DD');
         const eventList = events.filter(
             (event) =>
-                date.isSame(event.start, 'day') || date.isBefore(event.end, 'day')
+                moment(dateString).isBetween(event.start, event.end, 'day', '[]')
         );
+
         return (
-            <ul>
-                {eventList.map((event) => (
-                    <div className="h-3 w-3 bg-blue-500 rounded-full absolute bottom-1 left-1">
-                        <CheckCircleOutlined className="text-white w-2 h-2 absolute  [-translate-y-1/2]" />
+            <div>
+                {eventList.map((event, index) => (
+                    <div key={index} className="h-3 w-3 bg-blue-500 rounded-full absolute bottom-1 left-1">
+
                     </div>
                 ))}
-            </ul>
+            </div>
         );
     };
     const handleMenuClick = (e) => {
         setIsModalVisible(e.key);
     };
-    const handleModalOk = () => {
-        setIsModalVisible(false);
-    };
+
     const handleModalCancel = () => {
         setIsModalVisible(false);
     };
+
     const menu = (
         <Menu onClick={(e) => handleMenuClick(e)}>
             <Menu.Item  key="1">Exports</Menu.Item>
@@ -103,7 +107,7 @@ const Home = () => {
                     <p>Events on {selectedDate ? selectedDate.format('MMMM Do, YYYY') : ''}</p>
                     <Dropdown  onClick={(e) => e.preventDefault()} overlay={menu} placement="bottomLeft" trigger={['click']}>
                         <button className="ant-dropdown-link" onClick={(e) => e.preventDefault()}>
-                            + <i className="fas fa-caret-down"></i>
+                             <i className="fas fa-caret-down"> + </i>
                         </button>
                     </Dropdown>
                 </div>}
@@ -123,16 +127,17 @@ const Home = () => {
             <Modal
                 title="Modal Exports"
                 visible={isModalVisible === '1'}
-                onOk={handleModalOk}
+                footer={null}
                 onCancel={handleModalCancel}
             >
-               <FormExport/>
+               <FormExport ref={formRef} />
             </Modal>
 
             <Modal
                 title="Modal Title"
                 visible={isModalVisible === '2'}
-                onOk={handleModalOk}
+                okButtonProps={{ style: { display: 'none' } }} // Hide the default OK button
+                cancelButtonProps={{ style: { display: 'none' } }} // Hide the default Cancel button
                 onCancel={handleModalCancel}
             >
                 <p>Modal has 2 content goes here</p>
